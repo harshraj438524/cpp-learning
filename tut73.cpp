9a53277747344dcbb736e93019552c4931157d6c
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-01-30 18:51:19 +0530

    singly linked list

--- tut73.cpp
+++ tut73.cpp
@@ -1,16 +1,141 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-       
-            cout<<n-1<<endl;
-        
-
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data =x;
+        next=NULL;
     }
+};
+void myfunc(Node *head){ 
+    while(head!=NULL){
+        cout<<head->data<<endl;
+        head=head->next;
+    }
+};
+Node* insertAtStart(Node *head,int a){
+    Node *temp=new Node(a);
+    temp->next=head;
+    return temp;
+}
+
+Node* insertAtEnd(Node *head,int a){
+    Node *curr=head;
+    Node *temp=new Node(a);
+    if(head==NULL){
+        return temp;
+    }
+    while(curr->next!=NULL){
+        curr=curr->next;
+    }
+   curr->next=temp;
+    return head;
+}
+
+Node* deleteStart(Node *head){
+    if(head==NULL){
+        return NULL;
+    }
+    return head->next;
+}
+Node* deleteEnd(Node *head){
+    
+    if(head==NULL){
+        return NULL;
+    }
+    if(head->next==NULL){
+        return NULL;
+    }
+    Node *curr=head;
+    while(curr->next->next!=NULL){
+       curr=curr->next;
+    }
+    curr->next=NULL;
+    return head;
+};
+
+// int deleteatpos(Node *head,int x){
+//     if(head==NULL){
+//         return 0;
+//     }
+//     Node *temp=head;
+//     while(x-1){
+//         temp=temp->next;
+//         x--;
+//     }
+//     // if(temp->next==NULL){
+//     //     temp=NULL;
+//     //     return head;
+//     // }
+//     return temp->data;
+// }
+
+Node* deleteatposi(Node *head,int x){
+    if(head==NULL){
+        return NULL;
+    }
+    if(x==1){
+        return head->next;
+    }
+    Node *temp=head;
+    while(x-2){
+        temp=temp->next;
+        x--;
+    }
+    if(temp->next->next==NULL){
+        temp->next=NULL;
+        return head;
+    }
+    else{
+      temp->next=temp->next->next;
+    }
+    return head;
+}
+
+Node* deleteatposit(Node *head,int x){
+    if(head==NULL){
+        return NULL;
+    }
+    if(x==1){
+        return head->next;
+    }
+    Node *temp=head;
+    while(x-1){
+        temp=temp->next;
+        x--;
+    }
+    if(temp->next==NULL){
+        // temp=NULL;
+        cout<<temp->data;
+
+        return head;
+    }
+    else{
+      temp->next=temp->next->next;
+    }
+    return head;
+}
+int main(){
+    Node *head=NULL;
+    Node* temp1=new Node(123);
+    head->next=temp1;
+    // Node *temp2=new Node(43);
+    // temp1->next=temp2;
+    // head= insertAtStart(head,15);
+    // head=insertAtEnd(head,23);
+    // head=deleteStart(head);
+    // head=deleteEnd(head);
+    // int x=deleteatpos(head,5);
+    // head=deleteatposi(head,1);
+    // temp1=NULL;
+
+
+    // head=deleteatposit(head,3);
+    
+    // cout<<x<<" "<<"sbds"<<endl;
+   
+    myfunc(head);
+    //  cout<<head->data;
 return 0;
 }
--- tut74.cpp
+++ tut74.cpp
@@ -1,10 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-			cin>>x>>y;
-			cin>>arr[{x,y}];
-		}
+struct Node{
+    int data;
+    Node* next;
+	Node *prev;
+    Node(int x){
+        data =x;
+        next=NULL;
+		prev=NULL;
+    }
+};
+void printList(Node* head){
+	while(head!=NULL){
+       cout<<head->data<<endl;
+	   head=head->next;
 	}
-    int r = 3;
-    printCombination(arr, n, r);
+}
+
+Node *insertAtStart(Node* head,int x){
+	if(head==NULL){
+		return new Node(x);
+	}
+   Node *temp=new Node(x);
+   temp->next=head;
+   head->prev=temp;
+   return temp;
+}
+int main(){
+	Node* head=new Node(12);
+	Node* temp1=new Node(13);
+	Node* temp2=new Node(14);
+	// Node* temp3=new Node(15);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+   
+	head=insertAtStart(head,6);
+
+	 printList(head);
+
+
+
+return 0;
 }
